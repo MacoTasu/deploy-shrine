5d08239ec562af7a2a54ae54a901e0df70acb7ac
MacoTasu <makomacomako@me.com>
2014-11-12 17:05:01 +0900

    rokuyou or numberに応じて情報を返すmethodを追加

--- app/models/Revelation.scala
+++ app/models/Revelation.scala
@@ -24,4 +24,44 @@ object Revelations extends DAO {
   def insert(revelation: Revelation)(implicit s: Session) {
     Revelations += revelation
   }
+
+  def findRandomByRokuyou(rokuyou: Short)(implicit s: Session): Option[Revelation] = {
+    Revelations.filter(_.rokuyou === rokuyou).firstOption
+  }
+
+  def rokuyouType(rokuyou: String): Int = {
+    if (rokuyou == "大安") {
+      return 1;
+    } else if (rokuyou == "赤口") {
+      return 2;
+    } else if (rokuyou == "先勝") {
+      return 3;
+    } else if (rokuyou == "友引") {
+      return 4;
+    } else if (rokuyou == "先負") {
+      return 5;
+    } else if (rokuyou == "仏滅") {
+      return 6;
+    } else {
+      return 0;
+    }
+  }
+
+  def rokuyouText(number: Int): String = {
+    if (number == 1) {
+      return "大安";
+    } else if (number == 2) {
+      return "赤口";
+    } else if (number == 3) {
+      return "先勝";
+    } else if (number == 4) {
+      return "友引";
+    } else if (number == 5) {
+      return "先負";
+    } else if (number == 6) {
+      return "仏滅";
+    } else {
+      return "We--i";
+    }
+  }
 }
