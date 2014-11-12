166467894e58fa9f6bbb331280be3f88ce7f6cb6
MacoTasu <makomacomako@me.com>
2014-11-12 17:06:52 +0900

    外部APIを使って今日がどのrokuyouにあたるかわかるようにした

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,16 +5,30 @@ import play.api.mvc._
 import models._
 import play.api.libs.json._
 import play.api.db.slick._
+import java.util.Date
+import play.api.libs.ws._
+import play.api.Play.current
+import play.api.libs.ws.WS.url
+import play.api.libs.ws.Response
+import scala.concurrent._
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import ExecutionContext.Implicits.global
 
 object Application extends Controller {
   import Revelations.RevelationReads
   import Revelations.RevelationWrites
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
   def pray = DBAction { implicit request =>
+      // http://kayosystem.blogspot.jp/2010/07/web.html
+      // こちらのAPIを利用しています
+      val date = "%tY/%<tm/%<td" format new Date
+      val future: Future[WS.Response] = {
+        WS.url("http://jqreki.appspot.com/qreki/" + date).get()
+      }
+      val response     = Await.result(future, Duration.Inf).body
+      val json         = Json.parse(response)
+      val rokuyou_text = (json \ "rokuyo").as[String]
       Ok(views.html.json(Json.stringify(Json.toJson(Revelations.findById(1)))))
   }
 }
